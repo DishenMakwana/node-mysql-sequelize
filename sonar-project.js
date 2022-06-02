aa3c0ba38b2fbd1808ae376c0c0acb9893e865e1
DISHEN MAKWANA <dishenmakwana.dm@gmail.com>
2022-06-02 11:03:55 +0530

    ADD: testcases in exclusion in sonar

--- sonar-project.js
+++ sonar-project.js
@@ -7,7 +7,7 @@ sonarqubeScanner(
       'sonar.sources': './',
       // 'sonar.tests': './',
       'sonar.inclusions': '**', // Entry point of your code
-      // 'sonar.test.inclusions':
+      'sonar.exclusions': './tests/**',
       //   'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
       'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
       // 'sonar.testExecutionReportPaths': 'coverage/test-reporter.xml',
